03422233ce1c834e83877aff65811ed06fdd4763
PaulaEs <paulaproxy@gmail.com>
2013-11-25 00:56:25 +0100

    Spec for passwords validation and encryption.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,6 +60,37 @@ describe User do
     it "should have a password confirmation attribute" do
       @user.should respond_to(:password_confirmation)
     end
+  end
 
+  describe "password validations" do
+
+    it "should require a password" do
+      User.new(@attr.merge(password: "", password_confirmation: "")).should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(password_confirmation: "invalid")).should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(password: "short", password_confirmation: "short")
+      User.new(hash).should_not be_valid
+    end
+  end
+
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password attribute" do
+      @user.encrypted_password.should_not be_blank
+    end
   end
 end
